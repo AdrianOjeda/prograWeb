2b45c611bb8ad16d38d381199d10e894fd015644
AdrianOjeda <adrian.16300022@gmail.com>
2024-10-01 21:24:28 -0600

    Login and authentication using livewire implemente

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/test', function () {
     return view('test');
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
